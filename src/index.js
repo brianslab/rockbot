57f886d18f74f255cc2ce3ee24a9d78bb6a3970e
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-07 04:11:06 -0800

    setup bot per discord's docs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [] });
-const { DISCORD_TOKEN } = require('./tokens.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('../config.json');
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  client.commands.set(command.data.name, command);
+}
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on('message', (msg) => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true,
+    });
   }
 });
 
-client.login(DISCORD_TOKEN);
+client.login(token);
