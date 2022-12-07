74afce769f0ca2d74cc7e6c595cff68cefcda109
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-07 04:37:09 -0800

    finish spelltable command

--- src/commands/spelltable.js
+++ src/commands/spelltable.js
@@ -3,8 +3,16 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('spelltable')
-    .setDescription('Prints the link and the link appended with ?camera=true'),
+    .setDescription('Prints the link and the link appended with ?camera=true')
+    .addStringOption((option) =>
+      option
+        .setName('link')
+        .setDescription('The link to the Spelltable game.')
+        .setRequired(true)
+    ),
   async execute(interaction) {
-    await interaction.reply('Coming soon!');
+    const game = interaction.options.getString('link');
+    await interaction.reply(game + '\n' + game + '?camera=true');
+    // await interaction.followUp(game + '?camera=true');
   },
 };
